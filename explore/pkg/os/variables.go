2c65c7a01acd333eece3cf5cfad5d5764229d1d7
Method <git@zanuka.com>
2018-01-15 08:42:12 +1300

    constants and vars

---
+++ explore/pkg/os/variables.go
@@ -0,0 +1,7 @@
+var (
+        ErrInvalid    = errors.New("invalid argument") // methods on File will return this error when the receiver is nil
+        ErrPermission = errors.New("permission denied")
+        ErrExist      = errors.New("file already exists")
+        ErrNotExist   = errors.New("file does not exist")
+        ErrClosed     = errors.New("file already closed")
+)
